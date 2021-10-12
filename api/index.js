bdf3ac05a4d5749169e61df360661019c28a285f
Douglas Oliveira <douglaspo_97@outlook.com>
2021-10-11 22:00:10 -0300

    create middleware verify token and delete route

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
@@ -40,6 +41,34 @@ app.post('/api/login', (req, res) => {
     });
   } else {
     res.status(400).json('Username or password incorrect!');
+  }
+});
+
+//* Validando o token enviado no header da requisição (middleware)
+const verify = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, 'mySecretKey', (err, user) => {
+      if (err) {
+        return res.status(403).json('Token is not valid!');
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json('You are not authenticated!');
+  }
+};
+
+//* Criando rota delete com middleware
+app.delete('/api/users/:userId', verify, (req, res) => {
+  if (req.user.id === req.params.userId || req.user.isAdmin) {
+    res.status(200).json('User has been deleted!');
+  } else {
+    res.status(403).json('You are not allowed to delete this user!');
   }
 });
 
